a4a05ea41ff4bc818b5dfe93a3ddee438e4aee92
didieracm <82002809+didieracm@users.noreply.github.com>
2021-10-07 17:07:03 -0500

    se comentan migraciones en __init__

--- MoveAndFlowApp/models/__init__.py
+++ MoveAndFlowApp/models/__init__.py
@@ -1,7 +1,7 @@
 #cuando se crea un modelo, este debe poder exportarse a todo el proyecto para poder usarlo
 #su exportacion se hace de la siguiente manera indicando el archivo y la clase correspondiente
-# dentro de la carpeta models:
+# dentro de la carpeta models, dejo comentado mientras hya mas claridad al respecto:
 
-from .bicicletas import Bicicletas
-from .estaciones import Estaciones
-from .administrador import Administrador
+# from .bicicletas import Bicicletas
+# from .estaciones import Estaciones
+# from .administrador import Administrador
