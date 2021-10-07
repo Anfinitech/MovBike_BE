573aad493a7c9cd7438ceb6d88e517387d12a53d
didieracm <82002809+didieracm@users.noreply.github.com>
2021-10-07 16:49:39 -0500

    registro y exportacion de modelos segun guia de clase #8

--- MoveAndFlowApp/admin.py
+++ MoveAndFlowApp/admin.py
@@ -1,4 +1,16 @@
-from django.contrib import admin
-
 #Admin.py se utiliza para indicar a Django qué modelos utilizar en el proyecto
 # Register your models here.
+
+from django.contrib import admin
+
+#a continuacion el resgistro de los modelos en la aplicación, lo dejo comentado mientras hay mas 
+#claridad sobre ello, ver guia de clase #8
+
+# from .models.administrador import Administrador
+# from .models.bicicletas import Bicicletas
+# from .models.estaciones import Estaciones
+
+# admin.site.register(Bicicletas)
+# admin.site.register(Estaciones)
+# admin.site.register(Administrador)
+
