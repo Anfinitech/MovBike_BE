ba2d0b959be00defd82929bb86fdad733deb8a94
anfmedinara <anfmedinara@unal.edu.co>
2021-10-11 22:13:15 -0500

    CRUD + Pretty Stations Table

--- MoveAndFlowApp/serializers/estacionSerializer.py
+++ MoveAndFlowApp/serializers/estacionSerializer.py
@@ -6,14 +6,16 @@ from django.db.models import TextField
 
 
 class EstacionSerializer(serializers.ModelSerializer):
-    ocupacion = serializers.SerializerMethodField() 
-    bicicletasD = serializers.SerializerMethodField() 
-    bicicletasND = serializers.SerializerMethodField() 
-    bicicletasT = serializers.SerializerMethodField()  
+    e_estado = serializers.SerializerMethodField()
+    e_ocupacion = serializers.SerializerMethodField() 
+    e_bicicletasD = serializers.SerializerMethodField() 
+    e_bicicletasND = serializers.SerializerMethodField() 
+    e_bicicletasT = serializers.SerializerMethodField()  
 
     class Meta:    
         model = Estacion
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['e_id', 'e_nombre', 'e_estado', 'e_capacidad','e_ocupacion','e_bicicletasD','e_bicicletasND','e_bicicletasT']
 
         def create(self, validated_data):
             estacionInstance = Estacion.objects.create(**validated_data)
@@ -26,18 +28,21 @@ class EstacionSerializer(serializers.ModelSerializer):
                 "e_nombre" : estacion.e_nombre,
                 "e_estado" : estacion.e_estado,
                 "e_capacidad" : estacion.e_capacidad,
-            }
-        
-    def get_ocupacion(self,obj):
-        return str(round((Bicicleta.objects.filter(b_en_estacion=obj.e_id).count()*100/obj.e_capacidad),2))+"%"
+            }  
+
+    def get_e_estado(self,obj):
+        return "Abierta" if obj.e_estado else "Cerrada"
     
-    def get_bicicletasD(self,obj):
+    def get_e_ocupacion(self,obj):
+        return round((Bicicleta.objects.filter(b_en_estacion=obj.e_id).count()/obj.e_capacidad),4)
+    
+    def get_e_bicicletasD(self,obj):
         return (Bicicleta.objects.filter(b_en_estacion=obj.e_id).filter(b_condicion=True).count())
 
-    def get_bicicletasND(self,obj):
+    def get_e_bicicletasND(self,obj):
         return (Bicicleta.objects.filter(b_en_estacion=obj.e_id).filter(b_condicion=False).count())
 
-    def get_bicicletasT(self,obj):
+    def get_e_bicicletasT(self,obj):
         return (Bicicleta.objects.filter(b_en_estacion=obj.e_id).count())
 
 
