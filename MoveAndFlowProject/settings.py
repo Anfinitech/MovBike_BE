4dc4b77696f7f1b627ee234b16ef7dfb8c9233f2
anfmedinara <anfmedinara@unal.edu.co>
2021-10-12 18:57:24 -0500

    Corrected settings.py

--- MoveAndFlowProject/settings.py
+++ MoveAndFlowProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from .dbinfo import *
+
 from pathlib import Path
 from datetime import timedelta 
 
