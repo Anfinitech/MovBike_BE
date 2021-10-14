ad7312cc9f7cd250ac3775ef08719f722be5f07e
anfmedinara <anfmedinara@unal.edu.co>
2021-10-14 14:16:58 -0500

    corrected wsgi.py

--- MoveAndFlowProject/wsgi.py
+++ MoveAndFlowProject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoveAndFlowProject.settings_prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoveAndFlowProject.settings')
 
 application = get_wsgi_application()
