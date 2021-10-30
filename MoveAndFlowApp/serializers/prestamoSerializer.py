f84ba2b5e4f37453ad0a259f245031e33206e2db
ivonne <ivonne.suarez93@gmail.com>
2021-10-30 12:25:55 -0500

    Prestamo con modelo,serializador y vista

--- MoveAndFlowApp/serializers/prestamoSerializer.py
+++ MoveAndFlowApp/serializers/prestamoSerializer.py
@@ -1,20 +1,25 @@
 from MoveAndFlowApp.models.prestamo import Prestamo
 from rest_framework import serializers
+import datetime
+
+datetime.timedelta(hours=5)
+datetime.timedelta(5)
+
 
 
 class PrestamoSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Prestamo
-        fields = ['p_inicio', 'p_origen', 'p_destino']
+        fields = ['p_fin', 'p_origen', 'p_destino']
 
 
     def to_representation(self, obj):
         prestamo = Prestamo.objects.get(p_id=obj.p_id)
         return {
             "id" : prestamo.p_id,
-            "inicio" : prestamo.p_inicio,
-            "fin" : prestamo.p_fin,
-            "origen" : prestamo.p_origen,
-            "destino" : prestamo.p_destino,
+            "inicio" : prestamo.p_inicio - datetime.timedelta(hours=5),
+            "fin" : prestamo.p_fin - datetime.timedelta(hours=5),
+            "origen" : prestamo.p_origen.e_nombre,
+            "destino" : prestamo.p_destino.e_nombre,
         }
